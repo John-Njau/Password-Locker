74fe96fddaf13f7d2da0c94c16c3c62a94240a62
john.njau <john.njoroge@student.moringaschool.com>
2022-04-24 09:29:19 +0300

    ft; import user and creds class and defining their methods

--- credentials.py
+++ credentials.py
@@ -1,11 +1,42 @@
+from random import *
+
+
 class Credentials:
     """User credentials class, save information and generate passwords"""
-    def __init__(self,account_name, username, password):
+
+    def __init__(self, platform_name, username, password):
         """Initializing the credentials class"""
-        self.account_name = account_name
+        self.platform_name = platform_name
         self.username = username
         self.password = password
         #
+
     credentials_list = []
+
     def save_credentials(self):
+        """Method to save credentials"""
         Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """Method that allows a user to delete existing credentials"""
+        Credentials.credentials_list.remove(self)
+
+    def gen_password(ln):
+        """Generating a random password"""
+        chars = "ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*</>|"
+        password = ""
+        for i in range(ln):
+            password += chars[randint(0, len(chars) - 1)]
+        return password
+
+    @classmethod
+    def display_credentials(cls):
+        """Returns all saved credentials"""
+        return cls.credentials_list
+
+    @classmethod
+    def search_credentials(cls, platform_name):
+        """Method that takes in the platform name and searches for existing credentials in the credentials list"""
+        for credential in cls.credentials_list:
+            if credential.platform_name == platform_name:
+                return credential
