2dea52a256563afda4f4f6b68826bda7f699bd37
john.njau <john.njoroge@student.moringaschool.com>
2022-04-25 00:52:05 +0300

    rf: verify user method refactor

--- users.py
+++ users.py
@@ -3,19 +3,18 @@ class User:
     This is the users class
     """
 
-    def __init__(self, first_name, last_name, username, password):
+    user_accounts_list = []
+
+    def __init__(self, name, username, password):
         """
         Initialize user properties that will be required on 
         setting up a password locker account
         """
-        self.first_name = first_name
-        self.last_name = last_name
+        self.name = name
+        # self.last_name = last_name
         # self.email = email
         self.username = username
         self.password = password
-        self.login_attempts = 0
-
-    user_accounts_list = []
 
     def save_user_account(self):
         """saves user details to allow them to login later"""
@@ -25,12 +24,13 @@ class User:
         User.user_accounts_list.remove(self)
 
     @classmethod
-    def verify_user(cls, username, password):
+    def check_user(cls, username, password):
         current_user = " "
         for user in User.user_accounts_list:
             if user.username == username and user.password == password:
                 current_user = user.username
         return current_user
+
 # new_user = User("John", "Njau", "jd@mura.com")
 # print(new_user.__dict__)
 # print(new_user.__sizeof__())
